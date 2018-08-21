0a726b796c33a7f462d36f19defcc06bd01f6ac7
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-08-21 23:39:08 +0800

    1. setup configuration 2. added packages

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 require('dotenv-safe').config()
 
 const isDevMode = process.env.NODE_ENV === 'development'
@@ -7,9 +9,22 @@ const isProdMode = process.env.NODE_ENV === 'production'
 const spaceId = determineProperSpaceId()
 const token = process.env.STORYBLOK_MANAGEMENT_API_TOKEN
 
+const backupDirPath = path.resolve('./data/backup')
+const dataDirPath = determineProperDataDirPath()
+
 module.exports = {
+  backupDirPath,
+  dataDirPath,
   spaceId,
   token,
+}
+
+function determineProperDataDirPath() {
+  return isProdMode
+    ? path.resolve('./data/production')
+    : isStageMode
+      ? path.resolve('./data/staging')
+      : path.resolve('./data/development')
 }
 
 function determineProperSpaceId() {
