851d8cce544ae48c3234c8da0f94dba35706b2a5
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-09-26 22:33:56 +0800

    update code for more specific variable names

--- src/cliModules/starter/generatePhotos.js
+++ src/cliModules/starter/generatePhotos.js
@@ -19,55 +19,60 @@ module.exports = () => {
     read.workingData('photos'),
     read.stories('products'),
   ])
-    .then(([parentId, template, prodWorkData, photoWorkData, prodStories]) => {
-      workingData.products = prodWorkData
-      workingData.photos = filterValidPhotoSet(photoWorkData)
-      stories.products = prodStories
-      // generate contents for product photos
-      stories.photos = workingData.products.reduce((stories, product) => {
-        // find photos of this product
-        const filterFn = photo => photo.model === product.model
-        const productPhotos = workingData.photos.filter(filterFn)
-        // if no photos are found, do not add new entries
-        if (productPhotos.length === 0) return stories
-        else {
-          // generate new photos story content and add to stories
-          const partialSlug = 'model-' + slugify(product.model, { lower: true })
-          const story = Object.assign({}, template)
-          story.name = product.model
-          story.slug = partialSlug + '-photos'
-          story.parent_id = parentId
-          story.path = `catalog/products/${partialSlug}/`
-          story.content = Object.assign({}, template.content)
-          story.content.photoUrls = productPhotos.map(photo => {
-            return { component: 'photo url', url: photo.publicUrl }
-          })
-          stories.push(story)
-          return stories
-        }
-      }, [])
-      // create photo stories and asset creation on server
-      return Promise.all(
-        stories.photos.map(story => {
-          return createStory(story)
-            .then(story => {
-              logger.info(`"${story.name} photos" created`)
-              return story
+    .then(
+      ([
+        parentFolderId,
+        photosStoryTemplate,
+        productWorkingData,
+        photoWorkingData,
+        productStories,
+      ]) => {
+        workingData.products = productWorkingData
+        workingData.photos = filterValidPhotoSet(photoWorkingData)
+        stories.products = productStories
+        // generate contents for product photos
+        stories.photos = workingData.products.reduce((photoStories, product) => {
+          // find photos of this product
+          const filterFn = photo => photo.model === product.model
+          const productPhotos = workingData.photos.filter(filterFn)
+          // generate new photo story if there are photos for this product
+          if (productPhotos > 0) {
+            const partialSlug = 'model-' + slugify(product.model, { lower: true })
+            const story = Object.assign({}, photosStoryTemplate)
+            story.name = product.model
+            story.slug = partialSlug + '-photos'
+            story.parent_id = parentFolderId
+            story.path = `catalog/products/${partialSlug}/`
+            story.content = Object.assign({}, photosStoryTemplate.content)
+            story.content.photoUrls = productPhotos.map(photo => {
+              return { component: 'photo url', url: photo.publicUrl }
             })
-            .catch(error => Promise.reject(error))
-        })
-      )
-        .then(photoStories => {
-          stories.photos = photoStories
-          return buildRelationships(stories)
-        })
-        .then(() => {
-          // save the created stories to disk
-          const mappingFn = type => write.stories(type, stories[type])
-          return Promise.all(['photos', 'products'].map(mappingFn))
-        })
-        .catch(error => Promise.reject(error))
-    })
+          }
+          return photoStories
+        }, [])
+        // create photo stories and asset creation on server
+        return Promise.all(
+          stories.photos.map(story => {
+            return createStory(story)
+              .then(story => {
+                logger.info(`"${story.name} photos" created`)
+                return story
+              })
+              .catch(error => Promise.reject(error))
+          })
+        )
+          .then(photoStories => {
+            stories.photos = photoStories
+            return buildProductRelationships(stories)
+          })
+          .then(() => {
+            // save the created stories to disk
+            const mapFn = type => write.stories(type, stories[type])
+            return Promise.all(['photos', 'products'].map(mapFn))
+          })
+          .catch(error => Promise.reject(error))
+      }
+    )
     .then(() => logger.info('photos content generated'))
     .catch(error => Promise.reject(error))
 }
@@ -79,16 +84,16 @@ module.exports = () => {
  * @param {Object} stories.photos - photo content stories
  * @param {Object} stories.products - product content stories
  */
-function buildRelationships({ photos, products }) {
+function buildProductRelationships({ photos, products }) {
   // loop through each existing photos story
   return Promise.map(photos, photo => {
-    // find the product to link the current photo story to
-    const findIndexFn = product => product.content.model === photo.name
-    const index = products.findIndex(findIndexFn)
-    const story = products.splice(index, 1)[0] // find and extract
+    // find the product that is related to the current photo
+    const findProductIndexFn = product => product.content.model === photo.name
+    const productStoryIndex = products.findIndex(findProductIndexFn)
+    const productStory = products.splice(productStoryIndex, 1)[0]
     // update product story and put the story back into the list
-    story.content.photos = photo.id
-    return updateStory(story.id, story)
+    productStory.content.photos = photo.id
+    return updateStory(productStory.id, productStory)
       .then(updatedStory => products.push(updatedStory))
       .then(() => logger.info(`${photo.name} photos updated`))
       .catch(error => Promise.reject(error))
@@ -96,7 +101,7 @@ function buildRelationships({ photos, products }) {
 }
 
 /**
- * remove discared photos and map out the publicUrl to existing assets
+ * remove discarded photos and map out the publicUrl to existing assets
  *
  * @typedef {Object} PrepedPhotos
  * @property {string} PrepedPhotos.model - product model
@@ -109,10 +114,9 @@ function buildRelationships({ photos, products }) {
  * @returns {PrepedPhotos[]} prep'ed photos info
  */
 function filterValidPhotoSet(photos) {
-  return photos.filter(photo => !photo.reasonToDiscard).map(photo => {
-    return {
-      model: photo.model,
-      publicUrl: photo.publicUrl,
-    }
-  })
+  const filterFn = photo => !photo.reasonToDiscard
+  const mapFn = photo => {
+    return { model: photo.model, publicUrl: photo.publicUrl }
+  }
+  return photos.filter(filterFn).map(mapFn)
 }
