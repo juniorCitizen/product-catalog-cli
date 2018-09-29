bdbcc2342bd3743928db2aeefb0a6be30598ef70
Chia-Yu <chiayu.tsai.personal@gmail.com>
2018-09-29 14:56:28 +0800

    tooling config update

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// for vscode prettier extension
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'avoid',
+  proseWrap: 'never',
+}
